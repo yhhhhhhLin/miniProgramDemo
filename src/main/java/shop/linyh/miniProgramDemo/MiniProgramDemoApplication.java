7567d3d946337bba6b76a22ae8cf4239fd9c0fa9
linzz <1583427850@qq.com>
2024-10-17 23:24:34 +0800

    init project

---
+++ src/main/java/shop/linyh/miniProgramDemo/MiniProgramDemoApplication.java
@@ -0,0 +1,17 @@
+package shop.linyh.miniProgramDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+*
+* @author linzz
+*/
+@SpringBootApplication
+public class MiniProgramDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProgramDemoApplication.class, args);
+    }
+
+}
